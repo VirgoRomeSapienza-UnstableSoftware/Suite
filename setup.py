d32bc984f3a5b44d3b28315cba63ea6e43807b8b
Riccardo Felicetti <felicettiriccardo1@gmail.com>
2023-05-10 13:27:16 +0200

    auto versioning implemented

--- setup.py
+++ setup.py
@@ -12,10 +12,16 @@ here = pathlib.Path(__file__).parent.resolve()
 # Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
+
 setup(
     # Name of the project, registered the first time this was uploaded
     name="virgoSuite",  #   Required
-    # version="0.0.2",  #   Required
+    version_config=True,
+    version_config={
+        "template": "{tag}",
+    },
+    setup_requires=["setuptools-git-versioning"],
+    version="0.0.2",  #   Required
     description="Toolbox used from the data analysis group of Virgo Rome",  # Optional
     long_description=long_description,
     author="Riccardo Felicetti",
