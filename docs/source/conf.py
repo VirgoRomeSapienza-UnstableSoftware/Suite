4782ce881a73f0b130f759f625d14105d4f333c9
Riccardo Felicetti <felicettiriccardo1@gmail.com>
2023-05-10 17:32:55 +0200

    adding support for auto documentation hosting

---
+++ docs/source/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here.
+import sys, os
+
+sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../virgoSuite/"))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "virgoSuite"
+copyright = "2023, Virgo-Rome"
+author = "Felicetti Riccardo"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_automodapi.automodapi",
+]
+
+numpydoc_show_class_members = False
+numpydoc_class_members_toctree = False
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'furo'
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# -- Options for Cross-reference (links) -------------------------------------------------
+intersphinx_mapping = {
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
