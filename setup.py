079f09f06a5e30f1c904eb74a20ec7e4d88edb6f
Riccardo Felicetti <felicettiriccardo1@gmail.com>
2023-05-10 12:45:23 +0200

    added auto publish functionality

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+Setuptools based setup module
+
+this is used to upload to PyPi
+"""
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    # Name of the project, registered the first time this was uploaded
+    name="virgoSuite",  #   Required
+    version="0.0.1",  #   Required
+    description="Toolbox used from the data analysis group of Virgo Rome",  # Optional
+    long_description=long_description,
+    author="Riccardo Felicetti",
+    author_email="riccardo.felicetti@infn.it",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    packages=find_packages(include=["virgoSuie", "virgoSuite.*"]),
+)
